bb4a93b585bdd1d7c457e89aba19a724e7093f19
ninad-telang <ntelang@rivian.com>
2022-01-04 13:23:33 -0800

    Dynamic Values and Lists in JSX (App.js)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 function App() {
-    return <h1>Hello from the App Component.</h1>
+    const title = 'Feedback App'
+    const body = 'This is my body'
+    const comments = [
+        {id: 1, text: 'Comment One'},
+        {id: 2, text: 'Comment Two'},
+        {id: 3, text: 'Comment Three'},
+    ]
+    return (
+        <div className='container'>
+            <h1>{title.toUpperCase()}</h1>
+            <p>{body}</p>
+            <div className='comments'>
+                <h3>Comments = {comments.length}</h3>
+                <ul>
+                    {comments.map((comment) => (
+                        <li key={comment.id}>
+                            {comment.text}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default App
