bb62e34a1cbf9305070bb3ae3712477525fe80ef
ninad-telang <ntelang@rivian.com>
2022-01-04 13:49:51 -0800

    Conditionals in JSX (src/App.js)

--- src/App.js
+++ src/App.js
@@ -6,20 +6,27 @@ function App() {
         {id: 2, text: 'Comment Two'},
         {id: 3, text: 'Comment Three'},
     ]
+    const loading = false
+    const showComments = true
+    const commentBlock = (
+        <div className='comments'>
+            <h3>Comments = {comments.length}</h3>
+            <ul>
+                {comments.map((comment) => (
+                    <li key={comment.id}>
+                        {comment.text}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+
+    if (loading) return <h1>Loading...</h1>
     return (
         <div className='container'>
             <h1>{title.toUpperCase()}</h1>
             <p>{body}</p>
-            <div className='comments'>
-                <h3>Comments = {comments.length}</h3>
-                <ul>
-                    {comments.map((comment) => (
-                        <li key={comment.id}>
-                            {comment.text}
-                        </li>
-                    ))}
-                </ul>
-            </div>
+            {showComments && commentBlock}
         </div>
     )
 }
