a2087aa6f28249459ea0ff6ef9c77bfd7b558e7a
ninad-telang <ntelang@rivian.com>
2022-01-05 13:44:05 -0800

    Import Header component (App.js)

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
-function App() {
-    const title = 'Feedback App'
-    const body = 'This is my body'
-    const comments = [
-        {id: 1, text: 'Comment One'},
-        {id: 2, text: 'Comment Two'},
-        {id: 3, text: 'Comment Three'},
-    ]
-    const loading = false
-    const showComments = true
-    const commentBlock = (
-        <div className='comments'>
-            <h3>Comments = {comments.length}</h3>
-            <ul>
-                {comments.map((comment) => (
-                    <li key={comment.id}>
-                        {comment.text}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+import Header from './components/Header'
 
-    if (loading) return <h1>Loading...</h1>
+function App() {
     return (
-        <div className='container'>
-            <h1>{title.toUpperCase()}</h1>
-            <p>{body}</p>
-            {showComments && commentBlock}
-        </div>
+        <>
+            <Header text='Feedback-UI'/>
+            <div className='container'>
+                <h1>
+                    My App
+                </h1>
+            </div>
+        </>
     )
 }
 
