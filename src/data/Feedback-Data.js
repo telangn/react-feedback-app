f5f4e41e1a61fa4897e516a6f6c082fdc00633e3
ninad-telang <ntelang@rivian.com>
2022-01-06 13:46:03 -0800

    Initial Commit (Feedback-Data.js)

---
+++ src/data/Feedback-Data.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'Text here (id: 1, rating: 10)'
+    },
+    {
+        id: 2,
+        rating: 9,
+        text: 'Text here (id: 2, rating: 9)'
+    },
+    {
+        id: 3,
+        rating: 8,
+        text: 'Text here (id: 3, rating: 8)'
+    }
+]
+
+export default FeedbackData
