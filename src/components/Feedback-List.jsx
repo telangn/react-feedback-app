5292c017dd722c9bb79c383c029b3a4e3a84ee08
ninad-telang <ntelang@rivian.com>
2022-01-13 14:14:02 -0800

    handle delete (src/components/)

--- src/components/Feedback-List.jsx
+++ src/components/Feedback-List.jsx
@@ -13,7 +13,7 @@ function FeedbackList({feedback}) {
     return (
         <div className='feedback-list'>
             {feedback.map((item) => (
-                <FeedbackItem key={item.id} item={item}/>
+                <FeedbackItem key={item.id} item={item} handleDelete={(id) => console.log(id)}/>
             ))}
         </div>
     )
