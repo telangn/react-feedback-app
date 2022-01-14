662e18d0df7abce722f01a7893562e974bbd62bc
ninad-telang <ntelang@rivian.com>
2022-01-14 13:37:55 -0800

    Function drilling (Feedback-List.jsx)

--- src/components/Feedback-List.jsx
+++ src/components/Feedback-List.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import FeedbackItem from './Feedback-Item'
 
-function FeedbackList({feedback}) {
+function FeedbackList({feedback, handleDelete}) {
     if (!feedback || feedback.length === 0) {
         return (
             <p>
@@ -13,7 +13,7 @@ function FeedbackList({feedback}) {
     return (
         <div className='feedback-list'>
             {feedback.map((item) => (
-                <FeedbackItem key={item.id} item={item} handleDelete={(id) => console.log(id)}/>
+                <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
             ))}
         </div>
     )
