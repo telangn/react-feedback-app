6e92a1982097763bc01b1a803e7964cb394b175a
ninad-telang <ntelang@rivian.com>
2022-01-14 13:55:28 -0800

    Add attributes for testing (Feedback-Item.jsx)

--- src/components/Feedback-Item.jsx
+++ src/components/Feedback-Item.jsx
@@ -6,11 +6,11 @@ function FeedbackItem({item, handleDelete}) {
 
     return (
         <Card>
-            <div className='num-display'>{item.rating}</div>
-            <button onClick={() => handleDelete(item.id)} className='close'>
+            <div data-test-id='rating-number' className='num-display'>{item.rating}</div>
+            <button data-test-id='close-button' onClick={() => handleDelete(item.id)} className='close'>
                 <FaTimes color='purple' />
             </button>
-            <div className='text-display'>{item.text}</div>
+            <div data-test-id='feedback-text' className='text-display'>{item.text}</div>
         </Card>
     )
 }
