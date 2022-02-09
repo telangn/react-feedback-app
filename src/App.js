752336f040d875fe5cd8a4ba340af0471d5ee281
ninad-telang <ntelang@rivian.com>
2022-02-09 11:26:05 -0800

    Handle Adding a Feedback from FeedbackForm (App.js)

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {v4 as uuidv4} from 'uuid'
 import {useState} from 'react'
 import Header from './components/Header'
 import FeedbackForm from './components/FeedbackForm'
@@ -9,6 +10,11 @@ function App() {
 
     const [feedback, setFeedback] = useState(FeedbackData)
 
+    const addFeedback = (newFeedback) => {
+        newFeedback.id  = uuidv4()
+        setFeedback([newFeedback, ...feedback])
+    }
+
     const deleteFeedback = (id) => {
         if (window.confirm('Are you sure you want to delete')) {
             setFeedback(feedback.filter((item) => item.id !== id))
@@ -19,7 +25,7 @@ function App() {
         <>
             <Header text='Feedback-UI'/>
             <div className='container'>
-                <FeedbackForm></FeedbackForm>
+                <FeedbackForm handleAdd={addFeedback}/>
                 <FeedbackStats feedback={feedback}/>
                 <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
             </div>
